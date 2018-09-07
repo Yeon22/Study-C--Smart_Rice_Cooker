927b998606daf58d5e132b957e030a9a2edbc7cd
suyeon <bsuyeon16@gmail.com>
2018-09-07 15:05:28 +0900

    change text color infinity roop

--- SmartRiceCooker_1/Program.cs
+++ SmartRiceCooker_1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SmartRiceCooker_1
 {
@@ -10,6 +11,23 @@ namespace SmartRiceCooker_1
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(100, 40);
+            Random random = new Random();
+            ConsoleColor[] Color = { ConsoleColor.Blue, ConsoleColor.Cyan,
+                ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Green,
+                ConsoleColor.Magenta, ConsoleColor.Gray };
+
+            while (true)
+            {
+                Console.Clear();
+                for(int i = 0; i < 30; i++)
+                {
+                    Console.ForegroundColor = Color[random.Next(7)];
+                    Console.SetCursorPosition(random.Next(100), random.Next(40));
+                    Console.Write("Hello World!");
+                }
+                Thread.Sleep(200);
+            }
         }
     }
 }
